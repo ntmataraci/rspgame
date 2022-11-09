e3b78476528033714ca5418e32842c003b9ce767
Nevzat Mataraci <91319943+ntmataraci@users.noreply.github.com>
2022-11-09 23:14:10 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState} from 'react'
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
 export default function Home() {
   const randId=Math.floor(Math.random()*500)
 const [inputVal,setInputVal]=useState()  
@@ -10,12 +10,12 @@ const [inputVal,setInputVal]=useState()
   
   return (
     <div className="text-2xl text-slate-600 justify-center align-items-center flex w-[300px] h-[300px] m-auto flex-col">
-<a href="/single">Single Player</a>
+<Link href="/single">Single Player</Link>
 <h2 className='mt-5'>Multiplayer</h2>
-<a href={`/multi/${randId}`}>Host A game</a>
+<Link href={`/multi/${randId}`}>Host A game</Link>
 <div className='mt-5'>
 <input type="text" className='border-2 border-solid' value={inputVal} onChange={(e)=>setInputVal(e.target.value)} placeholder={"Please enter host number"}/>
-<a href={`/multi/${inputVal}`}>Join A game</a>
+<Link href={`/multi/${inputVal}`}>Join A game</Link>
 </div>
     </div>
   )
